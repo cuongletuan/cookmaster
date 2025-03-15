0bf5c2530f382bc6619890820e1a8550775d59c7
cuongle86 <tuancuongdesigner@gmail.com>
2025-03-15 20:59:29 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Poppins } from 'next/font/google';
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import TanStackClient from '@/provider/tanStackClient'
+import TanStackClient from "@/provider/tanStackClient";
+import Head from "next/head";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -22,10 +23,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable}`}
-      >
-        <TanStackClient> {children}</TanStackClient>  
+      <body className={`${poppins.variable}`}>
+        <TanStackClient>
+          {" "}
+          <Head>
+            <base href="/cookmaster/" />
+          </Head>
+          {children}
+        </TanStackClient>
       </body>
     </html>
   );
