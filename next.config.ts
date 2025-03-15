d81171c92f112180e59feb2c340316d1d86a0cb6
cuongle86 <tuancuongdesigner@gmail.com>
2025-03-15 20:41:06 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/cookmaster", 
-  assetPrefix: "cookmaster",
+  basePath: "/cookmaster",
+  assetPrefix: "/cookmaster/", 
 };
 
 export default nextConfig;
