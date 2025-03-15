e5ad09c873f16a7e92a43684d39a9e790f76d522
cuongle86 <tuancuongdesigner@gmail.com>
2025-03-15 23:46:48 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  assetPrefix: "https://cuongletuan.github.io/cookmaster/",
+
 };
 
 export default nextConfig;
